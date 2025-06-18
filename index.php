30c8183dc99ee8e55eb1ef2a404f1334a55e8d17
Fernante <fernandocorreo79@gmail.com>
2025-06-18 11:52:21 +0100

    primer commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Web</title>
+</head>
+<body>
+   <h1 style= "color:blue; text-align: center;">Bienvenido a mi primera web</h1> 
+   <p style= "color:blue; text-align: center;"><?php
+   echo "Hola, mundo! Esta es una página de ejemplo en PHP.";
+   ?></p>
+</body>
+
+</html>
