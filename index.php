a76d8558697dc285b6f44ec180cabc1a8d039f14
Fernante <fernandocorreo79@gmail.com>
2025-06-19 08:41:35 +0100

    siguiente

--- index.php
+++ index.php
@@ -11,6 +11,24 @@
    <p style= "color:blue; text-align: center;"><?php
    echo "Hola, mundo! Esta es una página de ejemplo en PHP.";
    ?></p>
+
+   <?php 
+   // comentarios en php
+   echo "</br>";
+   echo "Hola, mundo!";
+   echo "</br>";
+   echo "Este es otro mensaje"; 
+   echo "</br>"; 
+   // variables en php
+   $num1 = 10;
+   $num2 = 2.5;
+   echo "<h2>num1 incrementando: $num1, $num2 multiplicando por 2: $num2</h2>";
+   // Operaciones aritméticas
+   $suma = $num1 + $num2;
+   $resta = $num1 - $num2;
+   $multiplicacion = $num1 * $num2;
+   $division = $num1 / $num2;
+   ?>
 </body>
 
 </html>
